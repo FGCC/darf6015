31d754fb0efa4ccc357e443596441cf91cc05599
Fabio <35581008+FGCC@users.noreply.github.com>
2025-12-15 23:51:56 -0300

    sitemap-test-2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.darf6015.com.br',
-  output: 'static',
-  integrations: [sitemap()],
 });
