c20967aefc83ab6995e59fd788b5933fe38ee818
Fabio <35581008+FGCC@users.noreply.github.com>
2026-01-23 14:06:31 -0300

    remove inlinstylessheets

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,4 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://www.darf6015.com.br',
   trailingSlash: 'always',
-  build: {
-    inlineStylesheets: 'auto',
-  },
 });
