3fe4b8f0f06f96b4c511ce190f6b8bcea6d80166
Fabio <35581008+FGCC@users.noreply.github.com>
2026-04-29 20:22:36 -0300

    reverted-sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.darf6015.com.br',
-  integrations: [sitemap()],
 });
