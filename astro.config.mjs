216be34c809a0f28c2e707a57b0c107cfdf815cd
Fabio <35581008+FGCC@users.noreply.github.com>
2026-05-19 20:34:25 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://www.darf6015.com.br',
+  trailingSlash: 'always', // Force o Astro a sempre usar e gerar URLs com barra
 });
